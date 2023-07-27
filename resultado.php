9f5983f733dc4d327a9c7b3c80d7b887bfacb6cf
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-07-27 16:45:53 -0300

    pedro

--- resultado.php
+++ resultado.php
@@ -7,15 +7,19 @@
 </head>
 <body>
    <?php
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {    
             // Verifica se os campos foram preenchidos
             if (isset($_POST["lado1"]) && isset($_POST["lado2"]) && isset($_POST["lado3"])) {
-                $lad1 = $_POST["lado1"];
-                $lad2 = $_POST["lado2"];
-                $lad3 = $_POST["lado3"];
-            
-
-            } else {echo "<h2>Preencha todos os campos</h2>";}
+                $l1 = $_POST["lado1"];
+                $l2 = $_POST["lado2"];
+                $l3 = $_POST["lado3"];
+               if(($l1 <= ($l2 + $l3)) && ($l2 <= ($l1 + $l3)) && ($l3 <= ($l2 + $l1))){
+                echo "triangulo verdadeiro";
+                }else{
+                    echo "triangulo errado";
+                }
+            }else{
+            echo "<h2>Preencha todos os campos</h2>";}
         }
    ?>
 </body>
