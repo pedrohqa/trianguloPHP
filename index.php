d900bf3b8fadb88e898774f20b1820d089ce1c13
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-07-28 11:49:39 -0300

    pedro

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>triangulo</title>
-</head>
-<body>
-    <h1>Triangulos<h1>
-    <br>
-    <h3>Informe os Lados do Triangulo</h3>
-    <form action="resultado.php" method="post">
-        <label for="lado1">Lado1:</label>
-        <input type="number" name="lado1"><br>
-        <label for="lado2">Lado2:</label>
-        <input type="number" name="lado2"><br>     
-        <label for="lado3">Lado3:</label>
-        <input type="number" name="lado3"><br><br>
-        <input type="submit"class="bot">
-        <input type="reset" value="Reset" class="bot">
-    </form>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel='stylesheet' type='text/css' media='screen' href='triangulo.css'>
+        <title>triangulo</title>
+    </head>
+    <body>
+        <h1>Triangulos<h1>
+        <br>
+        <h4>Informe os Lados do Triangulo</h4>
+        <form action="resultado.php" method="post">
+            <div class="d1">
+            <label for="lado1">Lado1:</label>
+            <input type="number" name="lado1"><br>
+            <label for="lado2">Lado2:</label>
+            <input type="number" name="lado2"><br>     
+            <label for="lado3">Lado3:</label>
+            <input type="number" name="lado3"><br><br>
+            </div>
+            <input type="submit"class="bot1">
+            <input type="reset" value="Reset" class="bot2">
+        </form>
+    </body>
 </html>
--- resultado.php
+++ resultado.php
@@ -3,13 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel='stylesheet' type='text/css' media='screen' href='resultado.css'>
         <title>triangulo</title>
     </head>
     <body>
         <?php
             if ($_SERVER["REQUEST_METHOD"] === "POST"){    
                     // Verifica se os campos foram preenchidos
-                if (isset($_POST["lado1"]) && isset($_POST["lado2"]) && isset($_POST["lado3"])){
+                if (isset($_POST["lado1"]) && isset($_POST["lado2"]) && isset($_POST["lado3"])) {
                     $l1 = $_POST["lado1"];
                     $l2 = $_POST["lado2"];
                     $l3 = $_POST["lado3"];
@@ -28,9 +29,8 @@
                         echo "Não é um Triangulo";
                     }
                 }else{
-                        echo "<h2>Preencha todos os campos</h2>";
+                    echo "Preencha todos os campos";
                 }
-                
             }
         ?>
     </body>
