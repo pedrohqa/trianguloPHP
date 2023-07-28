7891f0d30e73f773fad05f955d99b195b85ba5bb
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-07-28 09:49:10 -0300

    pedro

--- resultado.php
+++ resultado.php
@@ -1,26 +1,37 @@
 <!DOCTYPE html>
 <html lang="br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>triangulo</title>
-</head>
-<body>
-   <?php
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {    
-            // Verifica se os campos foram preenchidos
-            if (isset($_POST["lado1"]) && isset($_POST["lado2"]) && isset($_POST["lado3"])) {
-                $l1 = $_POST["lado1"];
-                $l2 = $_POST["lado2"];
-                $l3 = $_POST["lado3"];
-               if(($l1 <= ($l2 + $l3)) && ($l2 <= ($l1 + $l3)) && ($l3 <= ($l2 + $l1))){
-                echo "triangulo verdadeiro";
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>triangulo</title>
+    </head>
+    <body>
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] === "POST"){    
+                    // Verifica se os campos foram preenchidos
+                if (isset($_POST["lado1"]) && isset($_POST["lado2"]) && isset($_POST["lado3"])){
+                    $l1 = $_POST["lado1"];
+                    $l2 = $_POST["lado2"];
+                    $l3 = $_POST["lado3"];
+                    if(($l1 <= ($l2 + $l3)) && ($l2 <= ($l1 + $l3)) && ($l3 <= ($l2 + $l1))){
+                   
+                        if(($l1 == ($l2 == $l3)) && (($l1 == $l2) == $l3) && ($l2 == ($l1 == $l3))){
+                            echo "<img src='img/equilatero.png'>";
+                        }
+                        elseif(($l2 == $l3) || ($l1 == $l3) || ($l2 == $l1)){
+                            echo "<img src='img/isosceles.png'>";
+                        }
+                        else{
+                            echo "<img src='img/escaleno.png'>";
+                        }
+                    }else{
+                        echo "Não é um Triangulo";
+                    }
                 }else{
-                    echo "triangulo errado";
+                        echo "<h2>Preencha todos os campos</h2>";
                 }
-            }else{
-            echo "<h2>Preencha todos os campos</h2>";}
-        }
-   ?>
-</body>
+                
+            }
+        ?>
+    </body>
 </html>
